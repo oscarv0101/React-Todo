72adb493f6436d9dea62c41e091a867eb34ae1da
milkslapper <oscar.valenzuela69@gmail.com>
2019-05-16 11:50:12 -0700

    added form with todo button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: <h1>Todo List: MVP</h1>,
+          id: 1,
+          completed: false
+        },
+        {
+          task: 'Learn setState()',
+          id: 2,
+          completed: false
+        },
+        {
+          task: 'Style my Todo List',
+          id: 3,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm/>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoForm = props => {
+    return (
+        <form>
+            <input 
+            placeholder="...todo"/>
+            <button>Add Todo</button>
+        </form>
+        
+    )
+}
+
+export default TodoForm;
